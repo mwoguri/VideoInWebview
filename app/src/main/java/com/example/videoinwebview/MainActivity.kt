42f6a7d32450a9f96f0f7a6237092e52208b34d3
Michiyo Wellington-Oguri <mwoguri@gmail.com>
2020-11-02 10:12:49 -0800

    video in webview can't be scrubbed

--- app/src/main/java/com/example/videoinwebview/MainActivity.kt
+++ app/src/main/java/com/example/videoinwebview/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.videoinwebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        WebView.setWebContentsDebuggingEnabled(true)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldInterceptRequest( view: WebView?, request: WebResourceRequest?
+            ): WebResourceResponse? {
+                if (request?.url.toString().contains("1.mp4")) {
+                    return WebResourceResponse(
+                        "video/mp4",
+                        "UTF-8",
+                        resources.openRawResource(R.raw.movie)
+                    )
+                }
+                return super.shouldInterceptRequest(view, request)
+            }
+        }
+
+        webView.loadDataWithBaseURL(
+            "http://fake.com",
+            """
+            <!DOCTYPE html>
+            <html>
+                <p>Video 1:</p>
+                <p><video src="http://fake.com/1.mp4" /></p>
+            </html>
+        """.trimIndent(), "text/html", StandardCharsets.UTF_8.name(), null
+        )
     }
 }
